78d835bac38e3c85bd714f3c79e51a5e5b7004ab
attila-ks <attila.ks@protonmail.com>
2023-08-31 11:09:37 +0200

    Move highlighting on yank config to a file

--- init.lua
+++ init.lua
@@ -1,16 +1,6 @@
 require("config.keymaps")
 require("config.options")
+require("config.yank")
 
 -- bootstrap lazy.nvim and your plugins
 require("config.lazy")
-
--- TODO: Try to put such configurations in separate files!
-
--- TODO: Check if this is the right way to achieve the highlighting!
--- [[ Highlight on yank ]]
-vim.api.nvim_create_autocmd("TextYankPost", {
-  pattern = "*",
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
---
+++ lua/config/yank.lua
@@ -0,0 +1,7 @@
+-- Highlight on yank.
+vim.api.nvim_create_autocmd("TextYankPost", {
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
