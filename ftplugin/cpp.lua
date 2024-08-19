a953a9548c3abb6dc9ae941137c9020a1b667654
attila-ks <attila.ks@protonmail.com>
2024-08-19 20:53:25 +0200

    Modify the config to use it in vscode

--- ftplugin/cpp.lua
+++
@@ -1,2 +0,0 @@
--- Switch between source/header (C/C++).
-vim.keymap.set("n", "<A-o>", ":ClangdSwitchSourceHeader<CR>", { silent = true })
--- lua/plugins/disabled.lua
+++
@@ -1,5 +0,0 @@
-return {
-  { "neo-tree.nvim", enabled = false },
-  { "dashboard-nvim", enabled = false },
-  { "tokyonight.nvim", enabled = false },
-}
