d56462ac59159567e8995ea8c7967ca01519cce3
attila-ks <attila.ks@protonmail.com>
2025-03-15 18:44:24 +0100

    Format files

--- lua/config/yank.lua
+++ lua/config/yank.lua
@@ -1,8 +1,8 @@
 -- Highlight on yank.
 vim.api.nvim_create_autocmd("TextYankPost", {
-    pattern = "*",
-  
-    callback = function()
-      vim.highlight.on_yank()
-    end,
-  })
+  pattern = "*",
+
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
