1509d6f7cbc16ffd09b7a7a15d737a7a5bc55296
attila-ks <attila.ks@protonmail.com>
2025-03-20 18:13:34 +0100

    Update keymaps

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -7,27 +7,9 @@ return {
       -- Toggle mappings in NORMAL mode.
       toggler = {
         -- Line-comment toggle keymap.
-        line = "<leader>cl",
+        line = "<leader>c",
         -- Block-comment toggle keymap.
-        block = "<leader>cb"
-      },
-
-      -- Operator-pending mappings in NORMAL and VISUAL mode.
-      opleader = {
-        -- Line-comment keymap.
-        line = "<leader>cL",
-        -- Block-comment keymap.
-        block = "<leader>cB"
-      },
-
-      -- Extra mappings.
-      extra = {
-        -- Add comment on the line above.
-        above = "<leader>cO",
-        -- Add comment on the line below.
-        below = "<leader>co",
-        -- Add comment at the end of line.
-        eol = "<leader>cA"
+        block = "<leader>bc"
       }
     }
   end
